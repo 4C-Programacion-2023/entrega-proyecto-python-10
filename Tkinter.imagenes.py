feef49248b1ea4d1848be00073a09120bdb7700a
quesyo <matiasd1708@gmail.com>
2023-08-23 15:53:04 -0300

    23 de agosto menu

--- Tkinter.imagenes.py
+++ Tkinter.imagenes.py
@@ -1,17 +1,26 @@
 from tkinter import*
 import tkinter as tk
+from tkinter import filedialog
 
-raiz=Tk()
-raiz.title("Camiseta x")
-raiz.resizable(False,False)
-raiz.geometry("400x600")
-raiz.config(bg="gray")
+window= Tk()
+window.title("MILAN RETRO")
+window.geometry("670x220")
+window.configure(bg="white")
 
-#creamos frame
-miFrame=Frame()
-miFrame.pack()
+image = tk.PhotoImage(file='./imagenes/2007 M.png')
+lb5 = tk.Label(window, image=image)
+lb5.place(x=0,y=0)
 
-#agregamos el fondo con una imagen
 
-miImagen=PhotoImage(file="imagenes.png")
-raiz.mainloop()
+
+image2 = tk.PhotoImage(file='./imagenes/Pirlo.png')
+lb5 = tk.Label(window, image=image2)
+lb5.place(x=250,y=0)
+
+image3 = tk.PhotoImage(file='./imagenes/1998 (milan).png')
+lb2 = tk.Label(window, image=image3)
+lb2.place(x=450,y=-3)
+
+
+
+window.mainloop()
